fabf863bad17226439b7ddf64c194cbbc845d925
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-07-04 16:53:46 +0300

    Brain-calc

--- index.js
+++ index.js
@@ -48,12 +48,68 @@ import readline from "readline-sync";
 //  }
 //}
 
-class EvenGame {
+//---------------------------------------------
+
+//class EvenGame {
+//  constructor() {
+//    this.name = "";
+//    this.counter = 0;
+//    this.randomNumber = Math.floor(Math.random() * 100);
+//    this.currentAnswer = this.randomNumber % 2 === 0 ? "yes" : "no";
+//    this.answer = this.currentAnswer;
+//  }
+//  getName() {
+//    this.name = readline.question(
+//      "Welcome to the Brain Games!\nMay I have your name?\n"
+//    );
+//    console.log(`Hello, ${this.name}`);
+//  }
+//  refreshValues() {
+//    this.randomNumber = Math.floor(Math.random() * 100);
+//    this.currentAnswer = this.randomNumber % 2 === 0 ? "yes" : "no";
+//  }
+//  play() {
+//    let firstAnswer = readline.question(
+//      `Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${this.randomNumber}\n`
+//    );
+//    if (firstAnswer === this.currentAnswer) {
+//      console.log("Correct!");
+//      this.counter++;
+//      while (this.counter < 3 && this.answer === this.currentAnswer) {
+//        this.refreshValues();
+//        this.answer = readline.question(`Question: ${this.randomNumber}\n`);
+//        this.counter++;
+//        this.answer === this.currentAnswer
+//          ? console.log("Correct!")
+//          : console.log(
+//              `Correct answer was ${this.currentAnswer}. Let's try again, ${this.name}!`
+//            );
+//        this.counter === 3 && console.log(`Congratulations, ${this.name}`);
+//      }
+//    } else {
+//      console.log(
+//        `Correct answer was ${this.currentAnswer}. Let's try again, ${this.name}!`
+//      );
+//    }
+//  }
+//}
+//const Bill = new EvenGame();
+//Bill.getName();
+//Bill.play();
+
+//-------------------------------------------------------
+
+class BrainCalc {
   constructor() {
-    this.name = "";
+    this.operations = ["+", "-", "*"];
+    this.firstNumber = Math.floor(Math.random() * 30);
+    this.secondNumber = Math.floor(Math.random() * 30);
+    this.randomExpression = `${this.firstNumber} ${
+      this.operations[Math.floor(Math.random()) * 3]
+    } ${this.secondNumber}`;
+    this.randomExpressionValue = eval(this.randomExpression);
     this.counter = 0;
-    this.randomNumber = Math.floor(Math.random() * 100);
-    this.currentAnswer = this.randomNumber % 2 === 0 ? "yes" : "no";
+    this.currentAnswer = this.randomExpressionValue;
     this.answer = this.currentAnswer;
   }
   getName() {
@@ -62,35 +118,33 @@ class EvenGame {
     );
     console.log(`Hello, ${this.name}`);
   }
-  refreshValues() {
-    this.randomNumber = Math.floor(Math.random() * 100);
-    this.currentAnswer = this.randomNumber % 2 === 0 ? "yes" : "no";
+
+  refresh() {
+    this.firstNumber = Math.floor(Math.random() * 30);
+    this.secondNumber = Math.floor(Math.random() * 30);
+    this.randomExpression = `${this.firstNumber} ${
+      this.operations[Math.floor(Math.random()) * 3]
+    } ${this.secondNumber}`;
+    this.randomExpressionValue = eval(this.randomExpression);
+    this.currentAnswer = this.randomExpressionValue;
   }
+
   play() {
-    let firstAnswer = readline.question(
-      `Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${this.randomNumber}\n`
-    );
-    if (firstAnswer === this.currentAnswer) {
-      console.log("Correct!");
+    this.getName();
+    console.log("What is the result of the expression?");
+    while (this.counter < 3 && this.answer === this.currentAnswer) {
+      this.refresh();
+      this.answer = +readline.question(`Question: ${this.randomExpression}\n`);
       this.counter++;
-      while (this.counter < 3 && this.answer === this.currentAnswer) {
-        this.refreshValues();
-        this.answer = readline.question(`Question: ${this.randomNumber}\n`);
-        this.counter++;
-        this.answer === this.currentAnswer
-          ? console.log("Correct!")
-          : console.log(
-              `Correct answer was ${this.currentAnswer}. Let's try again, ${this.name}!`
-            );
-        this.counter === 3 && console.log(`Congratulations, ${this.name}`);
-      }
-    } else {
-      console.log(
-        `Correct answer was ${this.currentAnswer}. Let's try again, ${this.name}!`
-      );
+      this.answer === this.currentAnswer
+        ? console.log("Correct!")
+        : console.log(
+            `'${this.answer}' is wrong answer ;(. Correct answer was '${this.currentAnswer}'.\nLet's try again, ${this.name}`
+          );
+      this.counter === 3 && console.log(`Congratulations, ${this.name}`);
     }
   }
 }
-const Bill = new EvenGame();
-Bill.getName();
-Bill.play();
+
+const Pers = new BrainCalc();
+Pers.play();
